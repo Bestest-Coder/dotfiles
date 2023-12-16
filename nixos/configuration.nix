b13c627de99ef88e96dd3e61b4afb666e9c6dee6
Bestest-Coder <techlord18@gmail.com>
2023-12-16 18:47:02 -0500

    updating base version

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -67,7 +67,7 @@ in
 
   home-manager.backupFileExtension = "bak";
   home-manager.users.bestest = {
-    home.stateVersion = "23.05";
+    home.stateVersion = "23.11";
     xdg.configFile."nvim".recursive = true;
     xdg.configFile."nvim".source = ../neovim;
     programs.zsh = {
@@ -113,6 +113,6 @@ in
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
+  system.stateVersion = "23.11"; # Did you read the comment?
 
 }
