ed28316d1e41a60453ce977973b95feb7a4938e6
Bestest-Coder <techlord18@gmail.com>
2023-12-16 15:01:28 -0500

    added obs

--- nixos/daily-driver/fun.nix
+++ nixos/daily-driver/fun.nix
@@ -5,6 +5,7 @@
   environment.systemPackages = with pkgs; [
     steam
     spotify
+    obs
   ];
 
   programs.steam = {
@@ -12,4 +13,11 @@
     remotePlay.openFirewall = true;
   };
   hardware.opengl.driSupport32Bit = true;
+
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    v4l2loopback
+  ];
+  boot.extraModprobeConfig = ''
+    options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
+  '';
 }
