84c7010de67ff0bc08472defe120823323ecc47d
Bestest-Coder <techlord18@gmail.com>
2023-12-26 16:33:26 -0500

    added multi-gpu options and custom desktop entries

--- nixos/daily-driver/home.nix
+++ nixos/daily-driver/home.nix
@@ -22,6 +22,25 @@
     xdg.configFile."hypr".source = ../../hyprland;
     xdg.configFile."fuzzel".source = ../../fuzzel;
     xdg.configFile."waybar".source = ../../waybar;
+    xdg.desktopEntries = {
+      firefox-private = {
+        name = "Firefox Private";
+        genericName = "Web Browser";
+        icon = "firefox";
+        exec = "firefox --private-window %U";
+        terminal = false;
+        categories = ["Application" "Network" "WebBrowser"];
+        mimeType = ["text/html" "text/xml"];
+      };
+      steam-prime = {
+        name = "steam on prime";
+        genericName = "Application";
+        icon = "steam";
+        exec = "DRI_PRIME=1 steam"; # assuming that the 2nd GPU is the better one to offload to
+        terminal = false;
+        categories = ["Game"];
+        mimeType = ["x-scheme-handler/steam" "x-scheme-handler/steamlink"];
+      };
   };
 
 }
