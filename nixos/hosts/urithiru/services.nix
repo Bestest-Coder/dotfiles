89be03c36765b1c9e20a966baefc8eec09a9e054
Bestest_Coder <techlord18@gmail.com>
2025-01-22 01:31:49 -0500

    fixed typo in murmur, also disabling for now

--- nixos/hosts/urithiru/services.nix
+++ nixos/hosts/urithiru/services.nix
@@ -2,10 +2,10 @@
 {
   services = {
     murmur = {
-      enable = true;
+      enable = false;
       package = pkgs.unstable.murmur;
       openFirewall = true;
-      environmentFile = config.age.secrets.murmur-env.age;
+      environmentFile = config.age.secrets.murmur-env.path;
       welcometext = "Welcome to Total Drama Murmur";
       password = "$MURMUR_PASSWORD"; # will get replaced from env file
       users = 10;
